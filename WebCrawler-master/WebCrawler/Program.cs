a428bf6cbe9d7e1921c1fd4816fb7f9a5ae3a87f
Joáo Pedro R. Rodrigues <joaopedrorrodrigues@hotmail.com>
2019-09-23 11:10:07 -0300

    Commit final

---
+++ WebCrawler-master/WebCrawler/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inicarCrawler();
+            Console.ReadLine();
+        }
+
+        public static string retornaVetorDeObjetoJSON(List<Ip> ips)
+        {
+            var arrayJson = JsonConvert.SerializeObject(ips);
+            return arrayJson;
+        }
+
+        private static async Task inicarCrawler()
+        {
+            try
+            {
+                var url = "https://proxyservers.pro/proxy/list/order/updated/order_dir/desc";
+                var httpClient = new HttpClient();
+                var html = await httpClient.GetStringAsync(url);
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+                var ips = new List<Ip>();
+
+                var paginacao =
+                htmlDocument.DocumentNode.Descendants("ul")
+                    .Where(node => node.GetAttributeValue("class", "").Equals("pagination")).ToList();
+
+                Int64 totpagina = Convert.ToInt64(paginacao.Select(x => x.Elements("li")).Last().Last().InnerText);
+            
+                for(int i=1;i<= totpagina;i++)
+                {
+                    var urlpag = "https://proxyservers.pro/proxy/list/order/updated/order_dir/desc/page/" + i;
+                    var htmlpag = await httpClient.GetStringAsync(urlpag);
+                    htmlDocument.LoadHtml(htmlpag);
+                
+                
+                var trs =
+                htmlDocument.DocumentNode.Descendants("tr")
+                    .Where(node => node.GetAttributeValue("valign", "").Equals("top")).ToList();
+
+                    foreach (var tr in trs)
+                    {
+                        var ip = new Ip
+                        {
+                            Address = tr.Descendants("td").Skip(1).FirstOrDefault().InnerText,
+                            Port = tr.Descendants("td").Skip(2).FirstOrDefault().InnerText,
+                            Country = tr.Descendants("td").Skip(3).FirstOrDefault().InnerText,
+                            Protocol = tr.Descendants("td").Skip(6).FirstOrDefault().InnerText,
+                        };
+
+                        ips.Add(ip);
+                    }
+                }
+                string arquivoJson = retornaVetorDeObjetoJSON(ips);
+                System.IO.File.WriteAllText(@"C:\IpsJson.json", arquivoJson);
+                Console.WriteLine("Sucesso....");
+                Console.WriteLine("Precione Enter para continuar...");
+                ConsoleKeyInfo keyinfor = Console.ReadKey(true);
+                if (keyinfor.Key == ConsoleKey.Enter)
+                {
+                    System.Environment.Exit(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
